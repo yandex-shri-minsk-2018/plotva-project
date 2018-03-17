90597d3093cefbba932d73e36b5223670adff0ab
Maryja Peredryj <chysapko@gmail.com>
2018-03-17 13:39:57 +0300

    Layout added

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './Layout.css';
+
+class Layout extends Component {
+  render() {
+    let {header, content, footer} = this.props;
+    return (
+      <div className="layout">
+        {header && (
+          <header className="layout__header">
+            {header}
+          </header>
+        )}
+
+        {content && (
+          <main className="layout__content">
+            {content}
+          </main>
+        )}
+
+        {footer && (
+          <footer className="layout__footer">
+            {footer}
+          </footer>
+        )}
+      </div>
+    );
+  }
+}
+
+export {Layout};
