34f84882c15200985389219b478c74155634d286
Vladislav Korbut <v.korbut8@gmail.com>
2018-03-17 16:42:27 +0300

    fixup! Add message component

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -6,9 +6,11 @@ const formatOptions = {
   minute: 'numeric',
 };
 
+const formatter = new Intl.DateTimeFormat('ru-RU', formatOptions)
+
 export const Message = ({ isMy, text, status = 'sent', time }) => {
   const date = new Date(time);
-  const timeFormatted = new Intl.DateTimeFormat('ru-RU', formatOptions).format(date);
+  const timeFormatted = formatter.format(date);
   return (
     <div className={`message-wrapper ${isMy ? 'message-wrapper_my' : ''}`}>
       <div className={`message ${isMy ? 'message_my' : ''}`}>
