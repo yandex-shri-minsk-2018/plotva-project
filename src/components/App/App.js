c09b8e85f090fae2f2637f3c42063432cd77d127
Anatoly Belobrovik <anatoliybelobrovik@gmail.com>
2018-03-29 14:27:46 +0300

    Fix app layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,44 +2,46 @@ import React, { Component } from 'react';
 import { Layout } from '../Layout/Layout';
 import { Contacts } from '../Contacts/Contacts';
 import { Header } from '../Header/Header';
-import { SearchInput } from "../SearchInput/SearchInput";
+import { SearchInput } from '../SearchInput/SearchInput';
 
 import './App.css';
 
 export class App extends Component {
   render() {
-    let page = "contacts",
-        title = "Contacts",
-        subtitle = "";
+    let page = 'contacts',
+      title = 'Contacts',
+      subtitle = '';
     return (
-
-      <Layout header={<Header type={page} title={title} subtitle={subtitle} />} content={
-        <div>
-          { page === "contacts" && <SearchInput/> }
-          <Contacts contacts = {
-            [
-              {
-                userName: 'Aaron A Aaronson',
-                content: 'Some messege',
-                contentType: 'messege',
-                time: '9:30',
-                size: 'medium',
-                icon: 'x',
-              },
-              {
-                userName: 'Aaron A Aaronson',
-                content: 'online',
-                contentType: 'online',
-                size: 'medium',
-              },
-              {
-                userName: 'Aaron A Aaronson',
-                content: 'Some messege',
-                contentType: 'messege',
-                size: 'medium',
-              },
-            ]}
-          />
+      <Layout
+        header={<Header type={page} title={title} subtitle={subtitle} />}
+        content={
+          <div>
+            {page === 'contacts' && <SearchInput />}
+            <Contacts
+              contacts={[
+                {
+                  userName: 'Aaron A Aaronson',
+                  content: 'Some messege',
+                  contentType: 'messege',
+                  time: '9:30',
+                  size: 'medium',
+                  icon: 'x',
+                },
+                {
+                  userName: 'Aaron A Aaronson',
+                  content: 'online',
+                  contentType: 'online',
+                  size: 'medium',
+                },
+                {
+                  userName: 'Aaron A Aaronson',
+                  content: 'Some messege',
+                  contentType: 'messege',
+                  size: 'medium',
+                },
+              ]}
+            />
+          </div>
         }
         footer={'footer'}
       />
