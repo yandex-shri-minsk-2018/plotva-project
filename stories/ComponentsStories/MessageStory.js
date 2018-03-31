9b6af6de790b7484888c92cf2152b9a03cdb06c5
Vladislav Sivitsky <vladosiv@gmail.com>
2018-03-31 17:15:47 +0300

    Code style fixes

--- stories/ComponentsStories/MessageStory.js
+++ stories/ComponentsStories/MessageStory.js
@@ -3,5 +3,5 @@ import { storiesOf } from '@storybook/react';
 import { Message } from '../../src/components/Message/Message';
 
 export default storiesOf('Message', module).add('Standart Message', () => (
-  <Message isMy text="И тебе здарова" status="sent" time={Date.now()} />
+  <Message isMy text='И тебе здарова' status='sent' time={Date.now()} />
 ));
