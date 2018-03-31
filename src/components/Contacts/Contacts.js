ffebbbaa9c6da2bc2e23c5e418bf63d8b3cca8d0
Maryja Peredryj <chysapko@gmail.com>
2018-03-31 16:34:42 +0300

    Added current user, error handling

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import { Contact } from '../Contact/Contact';
-import { MenuAction } from '../MenuAction/MenuAction';
 import { SectionTitle } from '../SectionTitle/SectionTitle';
 
 import './Contacts.css';
@@ -9,26 +8,9 @@ import addUserIcon from './images/add-user.svg';
 
 export class Contacts extends Component {
   render() {
-    let {type} = this.props;
-
     return (
       <React.Fragment>
-
-        {type !== "contactList" && (
-          <React.Fragment>
-            <Link to="/dialog">
-              <Contact
-                userName="Lena Cohen"
-                content="+65 8586 3216"
-                size="large"
-                contentType="messege"
-              />
-            </Link>
-            <SectionTitle title="Contacts" />
-            <MenuAction text="Invite Friends" icon={addUserIcon} />
-          </React.Fragment>
-        )}
-
+        <SectionTitle title="Contacts" />
         <div className="contacts">
           {this.props.contacts.map((contact, index) => (
             <Link key={index} to="/dialog"><Contact {...contact} /></Link>
