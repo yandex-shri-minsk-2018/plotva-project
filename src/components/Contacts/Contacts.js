6ee8ae4ac9f4c18f4cc8beb1efe61ead79fe2b76
Maryja Peredryj <chysapko@gmail.com>
2018-04-07 20:54:14 +0300

    Changed getCurrentUser from local state to redux; set header back btn background to Icon component
    Right redux implementation

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -8,13 +8,6 @@ export class Contacts extends Component {
   render() {
     return (
       <React.Fragment>
-          <Contact
-            userName="Lena Cohen"
-            content="+65 8586 3216"
-            size="large"
-            contentType="message"
-            link="/chat"
-          />
         <SectionTitle title="Contacts" />
         <div className="contacts">
           {this.props.contacts.map((contact, index) => (
