e16b2e16404e08f623e4f717418f459f22c0fa17
Igor Sanko <ig.sanko@gmail.com>
2018-04-14 00:12:14 +0300

    footer buttons reflect path and more pretty

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,20 +3,28 @@ import { Link } from 'react-router-dom'
 import FooterBtn from '../FooterBtn/FooterBtn.js';
 import './Footer.css';
 
+const chats = 'Chats',
+      contacts = 'Contacts';
 
 export class Footer extends Component {
-
 
   render() {
     return (
       <div className="footer">
         <Link to="/chats">
-          <FooterBtn icon='footer-chats' description='Chats'/>
+          <FooterBtn 
+            icon='footer-chats' 
+            description={chats}
+            active={this.props.path === chats? true : false}
+          />
         </Link>
         <Link to="/contacts">
-          <FooterBtn icon='footer-contacts' description='Contacts'/>
+          <FooterBtn 
+            icon='footer-contacts' 
+            description={contacts}
+            active={this.props.path === contacts? true : false}
+          />
         </Link>
-        {/*<FooterBtn onClick={this.props.changeTab} source={SettingsIcon} description='Settings'/>*/}
       </div>
     );
   }
