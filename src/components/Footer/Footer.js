3cee14e754168e457f52d6f8499d355cf98d8c9f
Igor Sanko <ig.sanko@gmail.com>
2018-04-23 23:55:52 +0300

    footer with 3 actie buttons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import FooterBtn from '../FooterBtn/FooterBtn.js';
 import './Footer.css';
 
+const chats = 'Chats';
+const contacts = 'Contacts';
+const settings = 'Settings';
 
 export class Footer extends Component {
-
-
   render() {
     return (
       <div className="footer">
         <Link to="/chats">
-          <FooterBtn icon='footer-chats' description='Chats'/>
+          <FooterBtn icon="footer-chats" description={chats} active={this.props.path === chats} />
         </Link>
         <Link to="/contacts">
-          <FooterBtn icon='footer-contacts' description='Contacts'/>
+          <FooterBtn icon="footer-contacts" description={contacts} active={this.props.path === contacts} />
         </Link>
         <Link to="/profile">
-          <FooterBtn icon='footer-settings' description='Settings'/>
+          <FooterBtn icon="footer-settings" description={settings} active={this.props.path === settings} />
         </Link>
       </div>
     );
